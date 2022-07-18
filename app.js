4e7869510e2f6458c3584b0ddfb07285863b3f51
AriyanShihab <devshihab111@gmail.com>
2022-07-18 17:19:12 +0600

    js erorr solved

--- app.js
+++
@@ -1,8 +0,0 @@
-// getting dom reffarence
-
-const toogeler = document.getElementById(`menu-toogeler`);
-const navRight = document.querySelector(`.nav-right`);
-
-toogeler.addEventListener(`click`, () => {
-    navRight.classList.toggle(`show`);
-});
